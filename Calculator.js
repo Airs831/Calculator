519354981fc8ec844a5c49c352506e80396a679c
Erin Ray Limbo <erinlimbo@erins-mbp.attlocal.net>
2019-08-17 23:55:20 -0700

    Cleaned up code by adding a [updateScreen] function which all functions use

--- Calculator.js
+++ Calculator.js
@@ -2,9 +2,14 @@ let answer = 0;
 let history = [];
 let results = document.querySelector('#screen');
 
+const updateScreen = function(answer){
+  console.log(history);
+  history.shift();
+  history.push(answer);
+  results.textContent = answer;
+  console.log(history);
+}
 
-
-console.log(results.textContent)
 const makeContentEditable = function() {
   if (results.textContent !== "0") {
     results.contentEditable = 'true';
@@ -12,10 +17,11 @@ const makeContentEditable = function() {
     results.contentEditable = 'false';
   };
 }
+
 let calculator = {
-  refreshScreen: false,
-  operatorInEffect: false,
-  input: function(button){
+  refreshScreen: false, // Resets Screen when operator is pressed. Wont allow operators to successively press
+  operatorInEffect: false, // Checks which operator has to update the screen
+  input: function(button){ // Inputs numbers
     if (calculator.refreshScreen == true){
       results.textContent = '';
       calculator.refreshScreen = false;
@@ -53,57 +59,22 @@ let calculator = {
   adding: function() {
     console.log('hi');
     let answer = Number(results.textContent) + history[0];
-    console.log(history);
-    history.pop();
-    history.pop();
-    history.push(answer);
-    results.textContent = answer;
-    console.log(history);
+    updateScreen(answer);
   },
   subtract:false,
   subtracting: function(){
-    if (calculator.subtract == true){
-      let answer = history[0] - Number(results.textContent);
-      history.pop();
-      history.pop();
-      history.push(answer);
-      results.textContent = answer;
-      console.log(history);
-    } else {
-      history.push(Number(results.textContent));
-      calculator.subtract = true;
-      results.textContent = '';
-    }
+    let answer = history[0] - Number(results.textContent);
+    updateScreen(answer);
   },
   multiply:false,
   multiplying:function(){
-    if (calculator.multiply == true){
-      let answer = history[0] * Number(results.textContent);
-      history.pop();
-      history.pop();
-      history.push(answer);
-      results.textContent = answer;
-      console.log(history);
-    } else {
-      history.push(Number(results.textContent));
-      calculator.multiply = true;
-      results.textContent = '';
-    }
+    let answer = history[0] * Number(results.textContent);
+    updateScreen(answer);
   },
   divide:false,
   dividing: function(){
-    if (calculator.divide == true){
-      let answer = history[0] / Number(results.textContent);
-      history.pop();
-      history.pop();
-      history.push(answer);
-      results.textContent = answer;
-      console.log(history);
-    } else {
-      history.push(Number(results.textContent));
-      calculator.divide = true;
-      results.textContent = '';
-    }
+    let answer = history[0] / Number(results.textContent);
+    updateScreen(answer);
   },
   isDecimal: false,
   decimal: function() {
@@ -125,41 +96,6 @@ let calculator = {
 
   answered:false,
 }
-
-//
-// let buttons = document.getElementsByClassName('button')
-// for (let i=0; i <buttons.length; i++) {
-//   let button = buttons[i];
-//   button.onclick = function(e){ //INPUT NUMBERS
-//     makeContentEditable();
-//     if (button.textContent < 10){
-//         calculator.input(button);
-//     } else if (button.id == 'ac') { //CLEAR SCREEN
-//       results.textContent = '';
-//       history.pop();
-//       console.log(history);
-//     } else if (button.id == 'plus' && calculator.operatorInEffect == false){ //ADD NUMBERS
-//       calculator.adding();
-//     } else if (button.id == 'minus' && calculator.operatorInEffect == false){ //SUBTRACKS NUMBERS
-//       calculator.subtracting()
-//     } else if (button.id == 'multiply' && calculator.operatorInEffect == false) { // MULTIPLIES NUMBERS
-//       calculator.multiplying();
-//     } else if (button.id == 'divide' && calculator.operatorInEffect == false){ //DIVIDE
-//       calculator.dividing();
-//     } else if (button.id == 'decimal'){
-//       calculator.decimal();
-//     } else if (button.id == 'percent'){
-//       calculator.percent();
-//     } else if (button.id == 'plusMinus'){
-//       calculator.plusMinus();
-//     }else if (button.id == 'equal') { //GETS ANSWER
-//       console.log(history);
-//       results.textContent = history[0];
-//   }
-//     console.log(button.id);
-//   }
-// }
-
 
 let buttons = document.getElementsByClassName('button')
 for (let i=0; i <buttons.length; i++) {
